a25d5107610f1e1e2867e19e58260d3eff61fa79
crupy <wlxh95@naver.com>
2023-05-15 16:39:29 +0900

    Init : 스프링 레거시 초기 생성 완료
    스프링 레거시 코드를 작성하기 위한 세팅 완료

---
+++ src/test/java/spring/legacy/LegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.legacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
